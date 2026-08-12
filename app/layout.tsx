364b7188eccb127ab93132de498f62e4ed753f96
RDPCHANNEL <110324176+RDPCHANNEL@users.noreply.github.com>
2026-08-12 21:03:15 +0700

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import './globals.css';
+
+export const metadata = {
+  title: 'Alight Motion Premium Generator',
+  description: 'Sistem Distribusi Otomatis Lisensi Akun Premium AM',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="id">
+      <body>{children}</body>
+    </html>
+  );
+}
+
